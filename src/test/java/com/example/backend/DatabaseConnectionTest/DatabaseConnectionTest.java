ab1ef08107e321b071712a5d97c63ceb6a27d615
Xunchi Zhang <ice_world@outlook.com>
2023-06-22 12:02:56 +0800

    🐞 fix(Test): 修复Connection测试的可用性
    在@DataJpaTest注解上添加@AutoConfigureTestDatabase，并设置replace属性为Replace.NONE，禁止用嵌入式数据库替换数据源。解决了因为没有嵌入式数据库导致的单元测试运行中的错误。

--- src/test/java/com/example/backend/DatabaseConnectionTest/DatabaseConnectionTest.java
+++ src/test/java/com/example/backend/DatabaseConnectionTest/DatabaseConnectionTest.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @DataJpaTest
 public class DatabaseConnectionTest {
 
