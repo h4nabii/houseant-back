1bbaa08e7b6845eed45b220261fe8315a282a051
shino <a1825580183@163.com>
2023-06-25 17:28:11 +0800

    数据库的一些内容:实现了CROS

--- src/main/java/com/example/backend/controller/UserController.java
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -3,11 +3,12 @@ package com.example.backend.controller;
 import com.example.backend.entity.User;
 import com.example.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class UserController {
 
