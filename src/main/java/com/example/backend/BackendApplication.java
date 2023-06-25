58512f1d8c74197c2601bfda30dbc3e59bca3704
h4nabii <leon_xenos.xu@foxmail.com>
2023-06-25 14:16:10 +0800

    reformat and removed duplicated dependency

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -12,27 +12,28 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
 
-	private final JdbcTemplate jdbcTemplate;
-	@Autowired
-	public BackendApplication(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BackendApplication(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args){
-		Logger logger = Logger.getLogger("test");
-		try {
-			jdbcTemplate.queryForObject("SELECT 1", Integer.class);
-			logger.log(Level.INFO, "Database connection test is successful.");
-		} catch (Exception e) {
-			logger.log(Level.INFO, "Database connection test failed.");
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void run(String... args) {
+        Logger logger = Logger.getLogger("test");
+        try {
+            jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+            logger.log(Level.INFO, "Database connection test is successful.");
+        } catch (Exception e) {
+            logger.log(Level.INFO, "Database connection test failed.");
+            e.printStackTrace();
+        }
+    }
 
 }
