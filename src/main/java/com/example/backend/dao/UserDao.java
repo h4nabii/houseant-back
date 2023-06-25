0c3e535451f240e9656a542eafbea433389d8a5b
BruceBatZhou.laptop <864292103@qq.com>
2023-06-25 16:23:05 +0800

    write some daos

--- src/main/java/com/example/backend/dao/UserDao.java
+++ src/main/java/com/example/backend/dao/UserDao.java
@@ -1,5 +1,18 @@
 package com.example.backend.dao;
 
+import com.example.backend.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Mapper
 public interface UserDao {
+    List<User> findAll();
+    List<User> findbykey(Map<String, Object> params);
+    void insert(User user);
+    void delete(Integer user_id);
+    void update(User user);
 
 }
