d3b70a44b086e5b0674efa21bbafb4e7d55c2b65
shino <a1825580183@163.com>
2023-06-25 16:30:07 +0800

    数据库的一些内容

--- src/main/java/com/example/backend/service/UserService.java
+++ src/main/java/com/example/backend/service/UserService.java
@@ -1,17 +1,14 @@
 package com.example.backend.service;
 
 import com.example.backend.entity.User;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 import java.util.Map;
-@Mapper
+
 public interface UserService {
     List<User> findAll();
-    List<User> findbykey(Map<String, Object> params);
+    List<User> findByKey(Map<String, Object> params);
     void insert(User user);
-    void delete(Integer user_id);
+    void delete(Integer userId);
     void update(User user);
-
 }
-
--- src/main/java/com/example/backend/service/impl/UsersServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.service.impl;
-
-public class UsersServiceImpl {
-
-}
