a0dc871ada165700c15a62bfaefb22414f557a95
shino <a1825580183@163.com>
2023-07-04 11:39:26 +0800

    完成了houseservice

--- src/main/java/com/houseant/backend/service/impl/HouseServiceImpl.java
+++ src/main/java/com/houseant/backend/service/impl/HouseServiceImpl.java
@@ -1,4 +1,45 @@
 package com.houseant.backend.service.impl;
 
-public class HouseServiceImpl {
+import com.houseant.backend.dao.HouseInfoDAO;
+import com.houseant.backend.entity.House;
+import com.houseant.backend.service.HouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class HouseServiceImpl implements HouseService {
+     private  final HouseInfoDAO houseInfoDAO;
+     @Autowired
+     public  HouseServiceImpl(HouseInfoDAO houseInfoDAO)
+     {
+         this.houseInfoDAO=houseInfoDAO;
+     }
+    @Override
+    public List<House> findAll() {
+        return houseInfoDAO.findAll();
+    }
+
+    @Override
+    public List<House> findByKey(Map<String, Object> params) {
+        return houseInfoDAO.findByKey(params);
+    }
+
+    @Override
+    public void insert(House house) {
+         houseInfoDAO.insert(house);
+
+    }
+
+    @Override
+    public void delete(Integer house_id) {
+        houseInfoDAO.delete(house_id);
+    }
+
+    @Override
+    public void update(House house) {
+        houseInfoDAO.update(house);
+    }
 }
