a91a7016f92b652bec74e0a94399293f8c5a9204
h4nabii <leon_xenos.xu@foxmail.com>
2023-07-05 10:38:08 +0800

    Mapper中添加了account字段

--- src/main/java/com/houseant/backend/controller/HouseOwnerController.java
+++ src/main/java/com/houseant/backend/controller/HouseOwnerController.java
@@ -1,6 +1,40 @@
 package com.houseant.backend.controller;
 
+import com.houseant.backend.entity.House;
+import com.houseant.backend.service.HouseService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/owner")
 public class HouseOwnerController {
+    private static final Logger logger = LogManager.getLogger(HouseOwnerController.class);
+
+    private final HouseService houseService;
+
+    @Autowired
+    public HouseOwnerController(HouseService houseService) {
+        this.houseService = houseService;
+    }
+
+    @ResponseBody
+    @PostMapping("/add-source")
+    public ResponseEntity<?> addSource(
+            @NonNull @RequestBody House house,
+            @NonNull HttpServletRequest request
+    ) {
+        logger.info("New Source from " + request.getRemoteAddr());
+
+
+
+        return null;
+    }
+
     //1.添加房源
     //  HouseService -->  insert
     //2.修改房源
