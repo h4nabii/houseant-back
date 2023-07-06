8f7975e3e897dd2e757d2bc7b3f811ccd8e82170
shino <a1825580183@163.com>
2023-07-06 13:16:37 +0800

    update autologin

--- src/main/java/com/houseant/backend/controller/HomePageController.java
+++ src/main/java/com/houseant/backend/controller/HomePageController.java
@@ -1,7 +1,10 @@
 package com.houseant.backend.controller;
 
 import com.houseant.backend.annotations.NoLogin;
+import com.houseant.backend.entity.User;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +15,14 @@ public class HomePageController {
 
     @NoLogin
     @GetMapping("/")
-    public ResponseEntity<?> home() {
+    public ResponseEntity<?> home( @NonNull HttpServletRequest request) {
         // Your processing code goes here...
         var responseMsg = new HashMap<String, Object>();
         responseMsg.put("auto-login",true);
+        var session=request.getSession();
+        if(session!=null) {
+            responseMsg.put("account", ((User)request.getSession().getAttribute("user")).getAccount());
+        }
         return ResponseEntity.ok().body(responseMsg);
     }
 }
